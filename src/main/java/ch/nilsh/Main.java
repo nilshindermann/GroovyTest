fb0bdb986da1f6c318052f533348d3d61de41c8b
Nils Hindermann <nilshindermann@gmail.com>
2024-01-09 12:22:57 +0100

    Update project

--- src/main/java/ch/nilsh/Main.java
+++ src/main/java/ch/nilsh/Main.java
@@ -1,5 +1,22 @@
-package ch.nilsh;public class Main {
+package ch.nilsh;
+
+import javax.swing.*;
+
+/**
+ * Simple main class
+ */
+public class Main {
+    /**
+     * Main entry point of our application
+     *
+     * @param args Arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("GroovyTest");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
