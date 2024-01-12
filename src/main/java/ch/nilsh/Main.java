81e3c97f0374c24caa1d850036922ddced227e35
Nils Hindermann <nilshindermann@gmail.com>
2024-01-12 13:59:29 +0100

    Window Title "GroovyTest (1.20.2)"

--- src/main/java/ch/nilsh/Main.java
+++ src/main/java/ch/nilsh/Main.java
@@ -12,7 +12,7 @@ public class Main {
      * @param args Arguments
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GroovyTest");
+        JFrame frame = new JFrame("GroovyTest (1.20.2)");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
         frame.setLocationRelativeTo(null);
